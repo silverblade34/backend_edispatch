bce1d9f9461adad616f81091f6e976f4d6f35a51
silverblade34 <mpacheco.tacay@gmail.com>
2024-06-21 16:58:56 -0500

    Proyecto inicializado y estructura establecida

---
+++ src/main/java/com/example/backend_edispatch/BackendEdispatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_edispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEdispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEdispatchApplication.class, args);
+	}
+
+}
